1f085f846a0cd13cc05b663e7172d7035fc4157d
Yue Yu <64169902+yuejwyu@users.noreply.github.com>
2020-09-03 21:27:09 -0400

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const TOKEN_KEY = 'TOKEN_KEY';
-export const API_ROOT = "https://around-179500.uc.r.appspot.com";
+export const API_ROOT = "***";
 
 export const GEO_OPTIONS = {
     enableHighAccuracy: true,
