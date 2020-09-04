bc8af1115adeb14a55c6f9756bea511bc87ecfe2
Yue Yu <64169902+yuejwyu@users.noreply.github.com>
2020-09-03 21:28:40 -0400

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -41,7 +41,7 @@ class Home extends Component {
                     </TabPane>
                     <TabPane tab="Map" key="3">
                         <AroundMap
-                            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD3CEh9DXuyjozqptVB5LA-dN7MxWWkr9s&v=3.exp&libraries=geometry,drawing,places"
+                            googleMapURL="***"
                             loadingElement={<div style={{ height: `100%` }} />}
                             containerElement={<div style={{ height: `600px` }} />}
                             mapElement={<div style={{ height: `100%` }} />}
